05ab11b029730772809ee66f7723d2e6289aaa4f
Podzalupenko Ochkovich <andrey5hard@gmail.com>
2025-03-01 12:18:11 +0300

    fix2

--- 2.php
+++ 2.php
@@ -1,11 +1,17 @@
 <?php
-$numbers = [1, 2, 3, 8, 9, 4, 2, 3, 9, 9];
-$unique_numbers = array_unique($numbers);
-arsort($unique_numbers);
-if (count($unique_numbers) >= 3) {
-  $keys = array_keys($unique_numbers);
-  $third_max_key = $keys[1]; 
-  $third_max = $unique_numbers[$third_max_key];
-
-  echo "Третье максимальное число: " . $third_max . PHP_EOL;
+$numbers = [1, 2, 9, 8, 0, 4, 2, 3, 7, 5];
+rsort($numbers);
+$count = 0;
+$previous = null;
+$third_max;
+foreach ($numbers as $number) {
+  if ($number !== $previous) {
+    $count++;
+    if ($count === 3) {
+      $third_max = $number;
+      break; 
+    }
+    $previous = $number;
+  }
 }
+  echo "Третье максимальное число: " . $third_max . PHP_EOL;
